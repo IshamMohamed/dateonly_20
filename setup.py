8ae41f16d7022d30b5fd72449dc3df81cc1795d6
Isham Mohamed <IshamMohamed@users.noreply.github.com>
2024-11-05 06:56:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ module = Extension(
     include_dirs=["/usr/include/python3.8"],
 )
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="dateonly_20",
     version="0.1.0",
     description="A package for encoding, decoding, and manipulating 20th centure dates with C-level performance.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Isham Mohamed",
     author_email="email@goes.here",
     ext_modules=[module],
