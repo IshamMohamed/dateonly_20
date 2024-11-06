8ba46c6b052f10695c7fc5bde768948cea71f447
Isham Mohamed <IshamMohamed@users.noreply.github.com>
2024-11-07 07:16:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dateonly_20",
-    version="0.1.471",
+    version="0.1.472",
     description="A package for encoding, decoding, and manipulating 20th centure dates with C-level performance.",
     long_description=long_description,
     long_description_content_type="text/markdown",
